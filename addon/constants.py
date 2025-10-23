960d3b2a119fcb40b3e41a80fb31929013ae0d2d
0x0501 <41354084+0x0501@users.noreply.github.com>
2025-10-23 10:28:05 +0800

    feat: update constants and remove unnecessary fields.

--- addon/constants.py
+++ addon/constants.py
@@ -1,8 +1,8 @@
-VERSION = "v6.3.6k"
-RELEASE_URL = "https://github.com/lixvbnet/Dict2Anki"
+VERSION = "v1.0.0"
+RELEASE_URL = "https://github.com/0x0501/Apora-Dict2Anki"
 VERSION_CHECK_API = "https://api.github.com/repos/lixvbnet/Dict2Anki/releases/latest"
-WINDOW_TITLE = f"Dict2Anki {VERSION}"
-MODEL_NAME = f"Dict2Anki"
+WINDOW_TITLE = f"Apora Dict2Anki {VERSION}"
+MODEL_NAME = f"Apora Dict2Anki"
 
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
 HEADERS = {"User-Agent": USER_AGENT}
@@ -15,92 +15,41 @@ ASSET_FILENAME_PREFIX = "MG"
 MODEL_FIELDS = [
     "term",
     "definition",
-    "definition_en",
-    "uk",
-    "us",
-    "phrase0",
-    "phrase1",
-    "phrase2",
-    "phrase_explain0",
-    "phrase_explain1",
-    "phrase_explain2",
-    "sentence0",
-    "sentence1",
-    "sentence2",
-    "sentence_explain0",
-    "sentence_explain1",
-    "sentence_explain2",
-    "sentence_speech0",
-    "sentence_speech1",
-    "sentence_speech2",
-    "pplaceHolder0",
-    "pplaceHolder1",
-    "pplaceHolder2",
-    "splaceHolder0",
-    "splaceHolder1",
-    "splaceHolder2",
+    "definition_cn",
+    "ipa",  # merge `us` and `uk` into ipa field
+    "context",  # merge `sentence*` into context
     "image",
     "pronunciation",
     "group",
-    "exam_type",
-    "modifiedTime",
 ]
+
 CARD_SETTINGS = [
-    "definition_en",
+    "definition_cn",
     "image",
     "pronunciation",
-    "phrase",
-    "sentence",
-    "exam_type",
+    "context",
 ]
 
 
 class FieldGroup:
     def __init__(self):
-        self.definition_en = "{{definition_en}}"
+        self.definition_cn = "{{definition_cn}}"
         self.image = "{{image}}"
         self.pronunciation = "{{pronunciation}}"
-        self.phrase = [
-            ("{{phrase0}}", "{{phrase_explain0}}", "{{pplaceHolder0}}"),
-            ("{{phrase1}}", "{{phrase_explain1}}", "{{pplaceHolder1}}"),
-            ("{{phrase2}}", "{{phrase_explain2}}", "{{pplaceHolder2}}"),
-        ]
-        self.sentence = [
-            (
-                "{{sentence0}}",
-                "{{sentence_explain0}}",
-                "{{splaceHolder0}}",
-                '<a onclick="this.firstChild.play()"><audio src="{{sentence_speech0}}"></audio>▶︎</a>',
-            ),
-            (
-                "{{sentence1}}",
-                "{{sentence_explain1}}",
-                "{{splaceHolder1}}",
-                '<a onclick="this.firstChild.play()"><audio src="{{sentence_speech1}}"></audio>▶︎</a>',
-            ),
-            (
-                "{{sentence2}}",
-                "{{sentence_explain2}}",
-                "{{splaceHolder2}}",
-                '<a onclick="this.firstChild.play()"><audio src="{{sentence_speech2}}"></audio>▶︎</a>',
-            ),
-        ]
-        self.exam_type = "{{exam_type}}"
+        self.context = "{{context}}"
 
     def toggleOff(self, field):
         if field not in CARD_SETTINGS:
             raise RuntimeError(
                 f"Unexpected field: {field}. Must be in {CARD_SETTINGS}!"
             )
-        if field == "phrase":
-            setattr(self, field, [("", "", ""), ("", "", ""), ("", "", "")])
-        elif field == "sentence":
-            setattr(self, field, [("", "", "", ""), ("", "", "", ""), ("", "", "", "")])
+        if field == "context":
+            setattr(self, field, "")
         else:
             setattr(self, field, "")
 
     def toString(self) -> str:
-        return f"definition_en={self.definition_en}, image={self.image}, pronunciation={self.pronunciation}, phrase={self.phrase}, sentence={self.sentence}"
+        return f"definition_cn={self.definition_cn}, image={self.image}, pronunciation={self.pronunciation}, context={self.context}"
 
     def __str__(self) -> str:
         return self.toString()
@@ -117,45 +66,18 @@ def normal_card_template_qfmt(fg: FieldGroup):
             <h1 class="term">{{{{term}}}}</h1>
             <span>{fg.pronunciation}</span>
             <div class="pronounce">
-                <span class="phonetic">UK[{{{{uk}}}}]</span>
-                <span class="phonetic">US[{{{{us}}}}]</span>
+                <span class="phonetic">UK[{{{{ipa}}}}]</span>
             </div>
             <div class="definition">Tap To View</div>
-            <div class="definition_en"></div>
+            <div class="definition_cn"></div>
         </td>
         <td style="width: 33%;">
         </td>
     </tr>
 </table>
 <div class="divider"></div>
-<table>
-    <tr><td class="phrase">{fg.phrase[0][0]}</td><td>{fg.phrase[0][2]}</td></tr>
-    <tr><td class="phrase">{fg.phrase[1][0]}</td><td>{fg.phrase[1][2]}</td></tr>
-    <tr><td class="phrase">{fg.phrase[2][0]}</td><td>{fg.phrase[2][2]}</td></tr>
-</table>
-<table>
-    <tr>
-        <td class="sentence">
-            {fg.sentence[0][0]}
-            {fg.sentence[0][3]}
-        </td>
-        <td>{fg.sentence[0][2]}</td>
-    </tr>
-    <tr>
-        <td class="sentence">
-            {fg.sentence[1][0]}
-            {fg.sentence[1][3]}
-        </td>
-        <td>{fg.sentence[1][2]}</td>
-    </tr>
-    <tr>
-        <td class="sentence">
-            {fg.sentence[2][0]}
-            {fg.sentence[2][3]}
-        </td>
-        <td>{fg.sentence[2][2]}</td>
-    </tr>
-</table>
+
+<p>{fg.context}</p>
 """
 
 
@@ -167,12 +89,10 @@ def normal_card_template_afmt(fg: FieldGroup):
         <h1 class="term">{{{{term}}}}</h1>
             <span>{fg.pronunciation}</span>
             <div class="pronounce">
-                <span class="phonetic">UK[{{{{uk}}}}]</span>
-                <span class="phonetic">US[{{{{us}}}}]</span>
+                <span class="phonetic">UK[{{{{ipa}}}}]</span>
             </div>
             <div class="definition">{{{{definition}}}}</div>
-            <div class="definition_en">{fg.definition_en}</div>
-            <div class="exam_type">{fg.exam_type}</div>
+            <div class="definition_cn">{fg.definition_cn}</div>
         </td>
         <td style="width: 33%;">
             {fg.image}
@@ -180,33 +100,7 @@ def normal_card_template_afmt(fg: FieldGroup):
     </tr>
 </table>
 <div class="divider"></div>
-<table>
-    <tr><td class="phrase">{fg.phrase[0][0]}</td><td>{fg.phrase[0][1]}</td></tr>
-    <tr><td class="phrase">{fg.phrase[1][0]}</td><td>{fg.phrase[1][1]}</td></tr>
-    <tr><td class="phrase">{fg.phrase[2][0]}</td><td>{fg.phrase[2][1]}</td></tr>
-</table>
-<table>
-    <tr>
-        <td class="sentence">
-            {fg.sentence[0][0]}
-            {fg.sentence[0][3]}
-        </td>
-        <td>{fg.sentence[0][1]}</td>
-    </tr>
-    <tr>
-        <td class="sentence">
-            {fg.sentence[1][0]}
-            {fg.sentence[1][3]}
-        </td>
-        <td>{fg.sentence[1][1]}</td>
-    </tr>
-    <tr>
-        <td class="sentence">
-            {fg.sentence[2][0]}
-            {fg.sentence[2][3]}
-        </td>
-        <td>{fg.sentence[2][1]}</td>
-    </tr>
+<p>{fg.context}</p>
 </table>
 """
 
@@ -219,10 +113,9 @@ def backwards_card_template_qfmt(fg: FieldGroup):
         <h1 class="term"></h1>
             <div class="pronounce">
                 <span class="phonetic">UK[Tap To View]</span>
-                <span class="phonetic">US[Tap To View]</span>
             </div>
             <div class="definition">{{{{definition}}}}</div>
-            <div class="definition_en">{fg.definition_en}</div>
+            <div class="definition_cn">{fg.definition_cn}</div>
         </td>
         <td style="width: 33%;">
             {fg.image}
@@ -230,16 +123,7 @@ def backwards_card_template_qfmt(fg: FieldGroup):
     </tr>
 </table>
 <div class="divider"></div>
-<table>
-    <tr><td class="phrase">{fg.phrase[0][2]}</td><td>{fg.phrase[0][1]}</td></tr>
-    <tr><td class="phrase">{fg.phrase[1][2]}</td><td>{fg.phrase[1][1]}</td></tr>
-    <tr><td class="phrase">{fg.phrase[2][2]}</td><td>{fg.phrase[2][1]}</td></tr>
-</table>
-<table>
-    <tr><td class="sentence">{fg.sentence[0][2]}</td><td>{fg.sentence[0][1]}</td></tr>
-    <tr><td class="sentence">{fg.sentence[1][2]}</td><td>{fg.sentence[1][1]}</td></tr>
-    <tr><td class="sentence">{fg.sentence[2][2]}</td><td>{fg.sentence[2][1]}</td></tr>
-</table>
+<p>{fg.context}</p>
 """
 
 
@@ -297,7 +181,7 @@ tr {
 PRON_TYPES = ["noPron", "BrEPron", "AmEPron"]
 
 
-def get_pronunciation(word: dict, preferred_pron: int) -> (int, bool):
+def get_pronunciation(word: dict, preferred_pron: int) -> tuple[int, bool]:
     """:return: pron_type: int, is_fallback: bool"""
     if preferred_pron == 0:
         return 0, False
